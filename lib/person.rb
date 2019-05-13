daad0a2ba6bd1016ff527b69f843e7a95bd0fbc7
Annia Forney <anniaforney@gmail.com>
2019-05-13 19:35:45 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,7 +5,8 @@ class Person
 :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes)
-    attributes.each{|key, value| puts "#{key} is #{value}".send}
+    attributes.each{|key, value| puts }
+    attribute.send 
   end
 
 end
